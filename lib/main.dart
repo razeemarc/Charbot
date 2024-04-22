88f1aa350ba14439eb5186751e5c259981f7c9b8
razeemarc <razeemarc@gmail.com>
2024-04-22 11:12:29 +0530

    chatbot UI updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_generative_ai/google_generative_ai.dart';
-import 'package:intl/intl.dart';
+import 'chatbot.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,178 +13,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const ChatScreen(),
+      home: const HomeScreen(),
     );
   }
 }
 
-class ChatScreen extends StatefulWidget {
-  const ChatScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
-  @override
-  State<ChatScreen> createState() => _ChatScreenState();
-}
-
-class _ChatScreenState extends State<ChatScreen> {
-  final TextEditingController _userInput = TextEditingController();
-
-  static const apiKey = "YOUR_API_KEY_HERE";
-  final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
-
-  final List<Message> _messages = [];
-
-  Future<void> sendMessage() async {
-    final message = _userInput.text;
-
-    setState(() {
-      _messages.add(Message(
-          isUser: true, message: message, date: DateTime.now()));
-    });
-
-    final content = [Content.text(message)];
-    final response = await model.generateContent(content);
-
-    setState(() {
-      _messages.add(Message(
-          isUser: false,
-          message: response.text ?? "",
-          date: DateTime.now()));
-    });
+  void _navigateToChatScreen(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const ChatScreen()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/chatbotBG.png"),
-            fit: BoxFit.cover,
-            colorFilter:
-            ColorFilter.mode(Colors.black.withOpacity(0.8), BlendMode.dstATop),
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: _messages.length,
-                itemBuilder: (context, index) {
-                  final message = _messages[index];
-                  return Messages(
-                    isUser: message.isUser,
-                    message: message.message,
-                    date: DateFormat('HH:mm').format(message.date),
-                  );
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 15,
-                    child: TextFormField(
-                      style: const TextStyle(color: Colors.white),
-                      controller: _userInput,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        labelText: 'Enter Your Message',
-                        labelStyle: const TextStyle(color: Colors.white),
-                        filled: true,
-                        fillColor: const Color(0xFF89A4B8),
-                      ),
-                    ),
-                  ),
-                  const Spacer(),
-                  IconButton(
-                    padding: const EdgeInsets.all(12),
-                    iconSize: 30,
-                    style: ButtonStyle(
-                      backgroundColor:
-                      MaterialStateProperty.all(const Color(0xFFE1A308)),
-                      foregroundColor: MaterialStateProperty.all(Colors.white),
-                      shape: MaterialStateProperty.all(const CircleBorder()),
-                    ),
-                    onPressed: () {
-                      sendMessage();
-                    },
-                    icon: const Icon(Icons.send),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text('Home'),
       ),
-    );
-  }
-}
-
-class Message {
-  final bool isUser;
-  final String message;
-  final DateTime date;
-
-  Message({
-    required this.isUser,
-    required this.message,
-    required this.date,
-  });
-}
-
-class Messages extends StatelessWidget {
-  final bool isUser;
-  final String message;
-  final String date;
-
-  const Messages({
-    Key? key,
-    required this.isUser,
-    required this.message,
-    required this.date,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(15),
-      margin: EdgeInsets.symmetric(vertical: 15).copyWith(
-        left: isUser ? 100 : 10,
-        right: isUser ? 10 : 100,
-      ),
-      decoration: BoxDecoration(
-        color: isUser ? const Color(0xFFE1A308) : Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: const Radius.circular(10),
-          bottomLeft: isUser ? const Radius.circular(10) : Radius.zero,
-          topRight: const Radius.circular(10),
-          bottomRight: isUser ? Radius.zero : const Radius.circular(10),
-        ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
         children: [
-          Text(message,
-            style: TextStyle(
-              fontSize: 16,
-              color: isUser ? Colors.white : Colors.black,
-            ),
-          ),
-          Text(
-            date,
-            style: TextStyle(
-              fontSize: 10,
-              color: isUser ? Colors.white : Colors.black,
+          // Your main content here
+          // Add any widgets you want to be behind the button
+
+          Positioned(
+            bottom: 20.0, // Adjust this value to change the button's vertical position
+            right: 20.0, // Adjust this value to change the button's horizontal position
+            child: FloatingActionButton(
+              onPressed: () {
+                _navigateToChatScreen(context);
+              },
+              child: const Icon(Icons.chat, color: Colors.white), // Change the icon color here
+              backgroundColor: const Color(0xFFE1A308), // Change the background color here
             ),
           ),
         ],
